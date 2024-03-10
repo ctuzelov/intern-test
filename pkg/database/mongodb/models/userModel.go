e60b83e98ef87449cf89e911b0a5a620673d1530
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-03-11 00:28:17 +0500

    new vars for task 6

--- pkg/database/mongodb/models/userModel.go
+++ pkg/database/mongodb/models/userModel.go
@@ -8,6 +8,9 @@ type User struct {
 	ID            primitive.ObjectID `bson:"_id"`
 	Name          *string            `json:"name" validate:"required,min=2,max=50"`
 	Email         *string            `json:"email" validate:"required,email"`
+	Number        *string            `json:"number" validate:"required"`
+	DateOfBirth   *string            `json:"date_of_birth" validate:"required"`
+	UserType      *string            `json:"user_type" validate:"required"`
 	Password      *string            `json:"password" validate:"required,min=6"`
 	Token         *string            `json:"token"`
 	Refresh_Token *string            `json:"refresh_token"`
