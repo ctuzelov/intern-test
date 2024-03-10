4d380e2d4638e0019e77770cd041b9fdc7b9eef8
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-03-11 03:46:56 +0500

    getting env vars

--- pkg/app.go
+++ pkg/app.go
@@ -2,13 +2,20 @@ package pkg
 
 import (
 	"auth-service/pkg/api/routes"
+	"fmt"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // Function that handles the initialization of the project
 func Run() {
+	err := godotenv.Load("./cmd/.env")
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
 	port := os.Getenv("PORT")
 
 	if port == "" {
--- pkg/database/mongodb/repository/databaseConnection.go
+++ pkg/database/mongodb/repository/databaseConnection.go
@@ -7,12 +7,18 @@ import (
 	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Function that handles mongoDB connection
 func DBinstance() *mongo.Client {
+	err := godotenv.Load("./cmd/.env")
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return nil
+	}
 	MongoDb := os.Getenv("MONGODB_URL")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
